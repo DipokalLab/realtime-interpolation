c3e0ffdcb7b404c6679986de801c6f0bde821eee
Dipokal HHJ <hhj@devent.kr>
2024-02-01 20:44:10 +0900

    feat: update range

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,51 @@ import { Graph } from "./components/Graph";
 function App() {
   const [randomValue, setRandomValue] = useState([0]);
   const [interpolationValue, setInterpolationValue] = useState([0]);
+  const [range, setRange] = useState({
+    min: 0,
+    max: 10,
+  });
+  const [animationFrame, setAnimationFrame] = useState<any>();
 
-  const getRandomValue = () => {
-    return Math.floor(Math.random() * 10);
+  const getRandomValue = (max: number, min: number) => {
+    return Math.floor(Math.random() * (max - min) + min);
   };
 
-  const interpolateArray = () => {};
+  const interpolateArray = () => {
+    if (randomValue.length <= 4) {
+      return randomValue[randomValue.length - 1];
+    }
+
+    const arrayLength = randomValue.length;
+
+    return randomValue[arrayLength - 1] / 3;
+  };
 
   const animate = () => {
-    requestAnimationFrame(animate);
+    cancelAnimationFrame(animationFrame);
 
-    const randomNumber = getRandomValue();
+    setAnimationFrame(requestAnimationFrame(animate));
+
+    const randomNumber = getRandomValue(range.max, range.min);
     randomValue.push(randomNumber);
 
     setRandomValue([...randomValue, randomNumber]);
-    setInterpolationValue([...randomValue, randomNumber]);
+  };
+
+  const handleChangeRange = (e: any) => {
+    setRange({
+      min: Number(e.target.value),
+      max: Number(e.target.value) + 10,
+    });
   };
 
   useEffect(() => {
     animate();
-  }, []);
+  }, [range]);
+
+  useEffect(() => {
+    setInterpolationValue([...interpolationValue, interpolateArray()]);
+  }, [randomValue]);
 
   return (
     <div
@@ -37,11 +62,13 @@ function App() {
         flexDirection: "column",
       })}
     >
+      <p>{JSON.stringify(range)}</p>
       <p>Row data:</p>
       <Graph value={randomValue}></Graph>
 
       <p>Interpolation data:</p>
       <Graph value={interpolationValue}></Graph>
+      <input type="range" onChange={handleChangeRange} />
     </div>
   );
 }
